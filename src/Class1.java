9c9aa9cd2b2162d1e0733972edb0f2bdcb6a6b34
Navin Rao <nunurao@gmail.com>
2015-01-27 17:13:55 -0500

    made a change after configuration

--- src/Class1.java
+++ src/Class1.java
@@ -1,9 +1,10 @@
 
 public class Class1 {
 
+	// made a change
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("What does the fox say?");
 	}
-
 }
